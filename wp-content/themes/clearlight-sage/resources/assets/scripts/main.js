2421914475458ee363471ac7ef9301f71c22060c
Alisa Herr <alisa@unitymakes.us>
2017-07-19 20:04:18 -0400

    Install WordPress and Sage starter theme

---
+++ wp-content/themes/clearlight-sage/resources/assets/scripts/main.js
@@ -0,0 +1,35 @@
+/** import external dependencies */
+import 'jquery';
+
+/** import local dependencies */
+import Router from './util/Router';
+import common from './routes/common';
+import home from './routes/home';
+import aboutUs from './routes/about';
+
+/**
+ * Web Font Loader
+ */
+var WebFont = require('webfontloader');
+
+WebFont.load({
+ google: {
+   families: ['Lato'],
+ },
+});
+
+/**
+ * Populate Router instance with DOM routes
+ * @type {Router} routes - An instance of our router
+ */
+const routes = new Router({
+  /** All pages */
+  common,
+  /** Home page */
+  home,
+  /** About Us page, note the change from about-us to aboutUs. */
+  aboutUs,
+});
+
+/** Load Events */
+jQuery(document).ready(() => routes.loadEvents());
