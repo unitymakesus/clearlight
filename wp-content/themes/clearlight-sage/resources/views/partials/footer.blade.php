08e7930ca61cba70c299ddef62435381b3772922
Andrew Marino <andy@unitywebagency.com>
2021-02-09 11:18:35 -0500

    footer updates with styles and responsive layout

--- wp-content/themes/clearlight-sage/resources/views/partials/footer.blade.php
+++ wp-content/themes/clearlight-sage/resources/views/partials/footer.blade.php
@@ -16,124 +16,95 @@
 @endif
 
 <footer>
-<div class="container-wide">
-  <div class="row flex-grid">
-  <div class="col"> 
-      @include('partials.footer-logos', [
-        'logos'     => [
-          [
-            'src'   => App\asset_path('images/esourcebook.png'),
-            'alt'   => 'eSourceBook',
-            'link'  => 'https://www.esourcebook.net/united-states/kernersville/glass-industry-supplier/clearlight-glass-and-mirror/',
+  <div class="footer-logos">
+    <div class="container">
+      <div class="row">
+        @include('partials.footer-logos', [
+          'logos'     => [
+            [
+              'src'   => App\asset_path('images/esourcebook.png'),
+              'alt'   => 'eSourceBook',
+              'link'  => 'https://www.esourcebook.net/united-states/kernersville/glass-industry-supplier/clearlight-glass-and-mirror/',
+            ],
+            [
+              'src'   => App\asset_path('images/sgcc-glass-blue.png'),
+              'alt'   => 'The Safety Glazing Certifiation Council',
+              'link'  => 'https://www.sgcc.org/',
+            ],
+            [
+              'src'   => App\asset_path('images/nahb.png'),
+              'alt'   => 'National Association of Home Builders',
+              'link'  => 'https://www.nahb.org/',
+            ],
+            [
+              'src'   => App\asset_path('images/dfi.png'),
+              'alt'   => 'Diamon Fusion International',
+              'link'  => 'https://dfisolutions.com/',
+            ],
+            [
+              'src'   => App\asset_path('images/homebuilders.png'),
+              'alt'   => 'Home Builders Association of Winston Salem',
+              'link'  => 'https://hbaws.net/',
+            ],
+            [
+              'src'   => 'http://seal-nwnc.bbb.org/seals/blue-seal-200-42-clearlight-glass-mirror-inc-152800337.png',
+              'alt'   => 'Better Business Bureau Review',
+              'link'  => 'https://www.bbb.org/us/nc/kernersville/profile/beveled-glass/clearlight-glass-mirror-inc-0503-152800337',
+            ],
           ],
-          [
-            'src'   => App\asset_path('images/sgcc-glass-blue.png'),
-            'alt'   => 'The Safety Glazing Certifiation Council',
-            'link'  => 'https://www.sgcc.org/',
-          ],
-          [
-            'src'   => App\asset_path('images/nahb.png'),
-            'alt'   => 'National Association of Home Builders',
-            'link'  => 'https://www.nahb.org/',
-          ],
-          [
-            'src'   => App\asset_path('images/dfi.png'),
-            'alt'   => 'Diamon Fusion International',
-            'link'  => 'https://dfisolutions.com/',
-          ],
-          [
-            'src'   => App\asset_path('images/homebuilders.png'),
-            'alt'   => 'Home Builders Association of Winston Salem',
-            'link'  => 'https://hbaws.net/',
-          ],
-          [
-            'src'   => 'http://seal-nwnc.bbb.org/seals/blue-seal-200-42-clearlight-glass-mirror-inc-152800337.png',
-            'alt'   => 'Better Business Bureau Review',
-            'link'  => 'https://www.bbb.org/us/nc/kernersville/profile/beveled-glass/clearlight-glass-mirror-inc-0503-152800337',
-          ],
-          
-       ]
-      ]) 
-      
+        ])
+      </div>
     </div>
   </div>
-
-  <div class="container-wide">
-     <div class="row">
-
-        <div class="col s3">
-          <a class="brand-logo hide-on-small-only" href="{{ home_url('/') }}" rel="home">
-            @if (has_custom_logo())
-              @php
-                $custom_logo_id = get_theme_mod( 'custom_logo' );
-                $logo = wp_get_attachment_image_src( $custom_logo_id , 'clearlight-logo' );
-                $logo_2x = wp_get_attachment_image_src( $custom_logo_id, 'clearlight-logo-2x' );
-              @endphp
-              <img data-src="{{ $logo[0] }}"
-                  srcset="{{ $logo[0] }} 1x, {{ $logo_2x[0] }} 2x"
-                  alt="{{ get_bloginfo('name', 'display') }}">
-            @else
-              {{ get_bloginfo('name', 'display') }}
-            @endif
-          </a>
+  <div class="container">
+    <div class="row">
+      <div class="col s12 m12 l3">
+        <a class="brand-logo" href="{{ home_url('/') }}" rel="home">
+          @if (has_custom_logo())
+            @php
+              $custom_logo_id = get_theme_mod( 'custom_logo' );
+              $logo = wp_get_attachment_image_src( $custom_logo_id , 'clearlight-logo' );
+              $logo_2x = wp_get_attachment_image_src( $custom_logo_id, 'clearlight-logo-2x' );
+            @endphp
+            <img data-src="{{ $logo[0] }}"
+                srcset="{{ $logo[0] }} 1x, {{ $logo_2x[0] }} 2x"
+                alt="{{ get_bloginfo('name', 'display') }}">
+          @else
+            {{ get_bloginfo('name', 'display') }}
+          @endif
+        </a>
+      </div>
+      <div class="col s12 m6 l2">
+        <div class="contact text-bold">
+          <ul>
+            <li>
+              <a href="tel:+1-336-993-7300">336-993-7300</a><br />
+                336-993-1431
+            </li>
+            <li>1318 Shields Road<br />Kernersville, NC 27284</li>
+            <li><a href="{{ get_permalink(get_page_by_path('about/contact')) }}" class="contact-us">Contact Us ›</a></li>
+          </ul>
         </div>
-          <div class="col s3">
-          <div class="contact">
-            <ul class="size-medium">
-              <li>
-                <a href="tel:+1-336-993-7300">336-993-7300</a><br />
-                 336-993-1431
-              </li>
-              <li>1318 Shields Road<br />Kernersville, NC 27284</li>
-              <li><a href="{{ get_permalink(get_page_by_path('about/contact')) }}" class="contact-us">Contact Us ›</a></li>
-            </ul>
-          </div>
-          </div>
-        <div class="col s3">
-          <div>
-            <p> We are consistently delivering to our 
-                customers throughout North Carolina, 
-                South Carolina, and Virgina.
-            </p>
-            <p> We will work with you to ship your project
-                anywhere you need. Please copntat us to
-                discuss shipping options.
-            </p>
-          </div>
-        </div>
-        <div class="col s3">
-        <a href="{{ get_permalink(get_page_by_path('about/contact')) }}"><img src="{{ App\asset_path('images/footer-service-area-map.png') }}" alt=""/></a>
-       </div>
-
+      </div>
+      <div class="col s12 m6 l3">
+        <p>We are consistently delivering to our customers throughout North Carolina, South Carolina, and Virgina.</p>
+        <p>We will work with you to ship your project anywhere you need. Please copntat us to discuss shipping options.</p>
+      </div>
+      <div class="col s12 m12 l4">
+        <a href="{{ get_permalink(get_page_by_path('about/contact')) }}">
+          <img src="{{ App\asset_path('images/footer-service-area-map.png') }}" alt="Contact Us"/>
+        </a>
       </div>
     </div>
-    </div>  
-      {{-- <div class="col s12 m7 l8">
-        {!! do_shortcode ('[wpgmza id="1"]') !!}
-      </div> --}}
-      
-    {{-- <div class="row">
-      <div class="col">
-        <div class="h6 uppercase">Proud Members Of:</div>
-      </div>
-    </div> --}}
-
-    
-    {{-- <div class="row">
-      <div class="col s12 center-align">
-        <div class="h6 uppercase">{{ __('Find Us On eSourceBook', 'sage') }}</div>
-        {{-- <img src="{{ App\asset_path('images/esourcebook-qr.png') }}" alt="" /> --}}
-      </div>
-    </div> 
-    <div class="row">
+    <div class="row copyright">
       <div class="col s12 m4">
         <a href="/wp-content/uploads/2019/09/X190904-Terms-and-Conditions.pdf">Terms &amp; Conditions</a> &nbsp; <a href="{{ get_permalink(get_page_by_path('privacy-policy')) }}">Privacy Policy</a>
       </div>
       <div class="col s12 m4 center-align">
         &copy; {{ current_time('Y') }} Clearlight Glass &amp; Mirror. All rights reserved.
       </div>
-      <div class="col s12 m4 right-align">
-        @include('partials.unity')
+      <div class="col s12 m4 text-right-l">
+        <a href="https://unitywebagency.com">Powered By Unity</a>
       </div>
     </div>
   </div>
