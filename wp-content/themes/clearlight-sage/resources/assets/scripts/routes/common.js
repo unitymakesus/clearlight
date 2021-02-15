2a19b64bfa7a8ae16c0d3a25de543c8281684d5a
Andrew Marino <andy@unitywebagency.com>
2021-02-15 10:44:00 -0500

    fix typo, spacing and run yarn build

--- wp-content/themes/clearlight-sage/resources/assets/scripts/routes/common.js
+++ wp-content/themes/clearlight-sage/resources/assets/scripts/routes/common.js
@@ -85,25 +85,24 @@ export default {
       })
     });
 
-    //Accordoin Script
-      var acc = document.getElementsByClassName("accordion");
-      var i;
+    // Accordion Script
+    var acc = document.getElementsByClassName('accordion');
+    var i;
 
-      for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-          /* Toggle between adding and removing the "active" class,
-          to highlight the button that controls the panel */
-          this.classList.toggle("active");
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener('click', function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle('active');
 
-          /* Toggle between hiding and showing the active panel */
-          var panel = this.nextElementSibling;
-          if (panel.style.display === "block") {
-            panel.style.display = "none";
-          } else {
-            panel.style.display = "block";
-          }
-    });
-}
-
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === 'block') {
+          panel.style.display = 'none';
+        } else {
+          panel.style.display = 'block';
+        }
+      });
+    }
   },
 };
