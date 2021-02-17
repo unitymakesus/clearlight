2993f69481b00b411288c36d52cbf7b0cf5af059
Jacquelineomollo <Jacquelineomollo@gmail.com>
2021-02-17 09:23:01 -0500

    removed accordion script

--- wp-content/themes/clearlight-sage/resources/assets/scripts/routes/common.js
+++ wp-content/themes/clearlight-sage/resources/assets/scripts/routes/common.js
@@ -83,26 +83,6 @@ export default {
       $input.on('focus', function() {
         $input.parent().addClass('active');
       })
-    });
-
-    // Accordion Script
-    var acc = document.getElementsByClassName('accordion');
-    var i;
-
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener('click', function() {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle('active');
-
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === 'block') {
-          panel.style.display = 'none';
-        } else {
-          panel.style.display = 'block';
-        }
-      });
-    }
+    });   
   },
 };
